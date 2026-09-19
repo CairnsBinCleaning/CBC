5448db7f6977f90566e31807fc12612e004e480e
CairnsBinCleaning <zacsbincleaning@gmail.com>
2026-09-19 18:39:16 +1000

    Update page.tsx

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -110,7 +110,7 @@ export default function Privacy() {
           <Link href="/prices">Prices</Link>
           <Link href="/terms">Terms &amp; Cancellation</Link>
         </nav>
-      <small className="madeBy">Created by Siezar DeWaal</small></footer>
+      <small className="madeBy">Created by <a href="/faq#siezar-dewaal">Siezar DeWaal</a></small></footer>
     </main>
   );
 }
